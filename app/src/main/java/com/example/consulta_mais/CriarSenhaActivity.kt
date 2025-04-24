5b7b25ca24f8dd0a96c8a07c6418624f99373fd4
charles.conceicao <charles.ngomes@hotmail.com>
2025-04-24 10:55:30 -0300

    tela esqueceu senha

---
+++ app/src/main/java/com/example/consulta_mais/CriarSenhaActivity.kt
@@ -0,0 +1,47 @@
+package com.example.consulta_mais
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.consulta_mais.ui.theme.ConsultaMaisTheme
+
+class CriarSenhaActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConsultaMaisTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting4(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview4() {
+    ConsultaMaisTheme {
+        Greeting4("Android")
+    }
+}
